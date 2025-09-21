31ad6af200e6aa153c48716088aa6dc731ba8ef9
German Andres Hernandez Lopez <bleem398@gmail.com>
2025-09-21 21:58:50 +0000

    Cambio_21

--- srv/sapbackend-exits.js
+++ srv/sapbackend-exits.js
@@ -1,14 +1,25 @@
 const cds = require("@sap/cds");
 
-module.exports = cds.service.impl(async function () {
-  const { Incidents } = this.entities;
-  const sapbackend = await cds.connect.to("sapbackend");
-  this.on("READ", Incidents, async (req) => {
-    return await sapbackend.tx(req).send({
-      query: req.query,
-      headers: {
-        Authorization: "Basic c2FwdWk1OnNhcHVpNQ==",
-      },
+// module.exports = cds.service.impl(async function(srv) {
+//   const { Incidents } = this.entities;
+//   const sapbackend = await cds.connect.to("sapbackend");
+//   srv.on("READ", Incidents, async (req) => {
+//     return await sapbackend.tx(req).send({
+//       query: req.query,
+//       headers: {
+//         Authorization: "Basic c2FwdWk1OnNhcHVpNQ==",
+//       },
+//     });
+//   });
+// });
+
+module.exports = async (srv) => {
+    const { Incidents } = this.entities;
+    const sapbackend = await cds.connect.to("sapbackend");
+    
+    srv.on(["READ"], Incidents, async (req) => {
+
+        let IncidentsQuery = SELECT.from(req.query.SELECT.from).limit(req.query.SELECT)
+
     });
-  });
-});
+};
